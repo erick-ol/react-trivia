27b77b62497996a7c6bfe4f7b33fa7d16053f066
Erick Santos <erickosantos.dev@gmail.com>
2021-09-13 17:48:05 -0300

    Fazendo design tela de ranking

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import RankingSvg from '../components/SVG/RankingSvg';
+import styles from './css/ranking.module.css';
 
 class Ranking extends Component {
   constructor() {
@@ -16,34 +18,48 @@ class Ranking extends Component {
   render() {
     const players = this.ranking();
     return (
-      <div>
-        <h2 data-testid="ranking-title">Ranking:</h2>
-        <ul>
-          {
-            players.map((player, index) => {
-              const { name, picture, score } = player;
-              return (
-                <li key={ index }>
-                  <img
-                    data-testid="header-profile-picture"
-                    src={ picture }
-                    alt={ name }
-                  />
-                  <p>
-                    <span data-testid={ `player-name-${index}` }>{ name }</span>
-                    <span data-testid={ `player-score-${index}` }>{ score }</span>
-                  </p>
-                </li>
-              );
-            })
-          }
-        </ul>
-        <Link to="/">
-          <button type="button" data-testid="btn-go-home">
-            Início
-          </button>
-        </Link>
-      </div>
+      <>
+        <div className={ styles.purple_aside }>
+          <div className={ styles.elements_purple }>
+            <div className={ styles.svg_ranking }>
+              <RankingSvg />
+            </div>
+            <div className={ styles.ranking_title }>
+              <h1>Ranking</h1>
+            </div>
+          </div>
+        </div>
+        <div className={ styles.white_side }>
+          <ul>
+            {
+              players.map((player, index) => {
+                const { name, picture, score } = player;
+                return (
+                  <li key={ index } className={ styles.ranking_ind }>
+                    <div className={ styles.person }>
+                      <img
+                        data-testid="header-profile-picture"
+                        src={ picture }
+                        alt={ name }
+                      />
+                      <h3 data-testid={ `player-name-${index}` } className={ styles.header_text }>{ name }</h3>
+                    </div>
+                    <h5 data-testid={ `player-score-${index}` } className={ styles.header_text }>
+                      Pontos:
+                      { score }
+                    </h5>
+                  </li>
+                );
+              })
+            }
+          </ul>
+          <Link to="/">
+            <button type="button" data-testid="btn-go-home">
+              Início
+            </button>
+          </Link>
+        </div>
+      </>
     );
   }
 }
