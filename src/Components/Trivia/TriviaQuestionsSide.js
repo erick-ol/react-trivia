c8a228f9e193b3eab6a1d9b66cf576890f10189a
Erick Santos <erickosantos.dev@gmail.com>
2022-05-09 23:02:58 -0300

    feat: 🎸 Adding Questions Side Component

---
+++ src/Components/Trivia/TriviaQuestionsSide.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Header from '../Header';
+import nextImg from '../../assets/img/next_black.png';
+import styles from '../../Pages/css/trivia.module.css';
+import { addAssertion, addPoints } from '../../store/player';
+import {
+  decreaseSeconds,
+  increaseId,
+  resetSeconds,
+  setAnswered,
+} from '../../store/trivia';
+
+const TriviaQuestionsSide = () => {
+  const { data } = useSelector((state) => state.questions);
+  const { seconds, id, answered } = useSelector((state) => state.trivia);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (data && !answered && seconds > 0) {
+      setTimeout(() => dispatch(decreaseSeconds()), 1000);
+    }
+    if (!answered && seconds === 0) dispatch(setAnswered());
+  }, [seconds, answered, dispatch, data]);
+
+  const sumScore = () => {
+    dispatch(setAnswered());
+    const difficulty = data[id].difficulty;
+
+    switch (window.atob(difficulty)) {
+      case 'easy':
+        dispatch(addPoints(10 + seconds));
+        break;
+      case 'medium':
+        dispatch(addPoints(10 + seconds * 2));
+        break;
+      case 'hard':
+        dispatch(addPoints(10 + seconds * 3));
+        break;
+      default:
+        return;
+    }
+    dispatch(addAssertion());
+  };
+
+  const answeredStyle = (className) => {
+    if (className === 'correct') {
+      return answered ? styles.correct : '';
+    }
+    return answered ? styles.incorrect : '';
+  };
+
+  const progressBar = () => {
+    switch (id) {
+      case 0:
+        return styles.question_0;
+      case 1:
+        return styles.question_1;
+      case 2:
+        return styles.question_2;
+      case 3:
+        return styles.question_3;
+      case 4:
+        return styles.question_4;
+      default:
+        return 0;
+    }
+  };
+
+  const next = () => {
+    dispatch(increaseId());
+    dispatch(setAnswered());
+    dispatch(resetSeconds());
+  };
+
+  const linkOrNext = () => {
+    if (id === 4) navigate('/feedback');
+    else next();
+  };
+
+  return (
+    <>
+      {data && (
+        <div className={styles.white_side}>
+          <Header />
+          <div className={styles.questions_main}>
+            <p className={styles.question}>
+              <span>{window.atob(data[id].question)}</span>
+            </p>
+            <ul>
+              <li>
+                <button
+                  type="button"
+                  onClick={sumScore}
+                  className={`${answeredStyle('correct')} ${styles.answer}`}
+                  disabled={answered}
+                >
+                  {window.atob(data[id].correct_answer)}
+                </button>
+              </li>
+              {data[id].incorrect_answers.map((incorrect, i) => (
+                <li key={i}>
+                  <button
+                    type="button"
+                    onClick={() => dispatch(setAnswered())}
+                    className={`${answeredStyle('incorrect')} ${styles.answer}`}
+                    disabled={answered}
+                  >
+                    {window.atob(incorrect)}
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <div className={styles.progress_bar}>
+              <div className={progressBar()}></div>
+            </div>
+            {answered && (
+              <button
+                type="button"
+                onClick={linkOrNext}
+                data-testid="btn-next"
+                className={styles.next}
+              >
+                Próxima
+                <img src={nextImg} alt="proxima" />
+              </button>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default TriviaQuestionsSide;
--- src/Pages/Trivia.js
+++ src/Pages/Trivia.js
@@ -1,151 +1,29 @@
 import React from 'react';
-import Header from '../Components/Header';
-import Loading from '../Components/SVG/Loading';
-import styles from './css/trivia.module.css';
-import nextImg from '../assets/img/next_black.png';
-import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { addAssertion, addPoints } from '../store/player';
+
+import Loading from '../Components/SVG/Loading';
 import { fetchQuestions } from '../store/questions';
-import {
-  decreaseSeconds,
-  increaseId,
-  resetSeconds,
-  resetTrivia,
-  setAnswered,
-} from '../store/trivia';
+import { resetTrivia } from '../store/trivia';
 import TriviaTimeSide from '../Components/Trivia/TriviaTimeSide';
+import TriviaQuestionsSide from '../Components/Trivia/TriviaQuestionsSide';
 
 const Trivia = () => {
   const { loading, data } = useSelector((state) => state.questions);
-  const { seconds, id, answered } = useSelector((state) => state.trivia);
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   React.useEffect(() => {
+    if (!data) dispatch(fetchQuestions());
+
     return () => {
       dispatch(resetTrivia());
     };
-  }, [dispatch]);
-
-  React.useEffect(() => {
-    if (!data) dispatch(fetchQuestions());
   }, [data, dispatch]);
-
-  React.useEffect(() => {
-    if (data && !answered && seconds > 0) {
-      setTimeout(() => dispatch(decreaseSeconds()), 1000);
-    }
-    if (!answered && seconds === 0) dispatch(setAnswered());
-  }, [seconds, answered, dispatch, data]);
-
-  const sumScore = () => {
-    dispatch(setAnswered());
-    const difficulty = data[id].difficulty;
-
-    switch (window.atob(difficulty)) {
-      case 'easy':
-        dispatch(addPoints(10 + seconds));
-        break;
-      case 'medium':
-        dispatch(addPoints(10 + seconds * 2));
-        break;
-      case 'hard':
-        dispatch(addPoints(10 + seconds * 3));
-        break;
-      default:
-        return;
-    }
-    dispatch(addAssertion());
-  };
-
-  const answeredStyle = (className) => {
-    if (className === 'correct') {
-      return answered ? styles.correct : '';
-    }
-    return answered ? styles.incorrect : '';
-  };
-
-  const progressBar = () => {
-    switch (id) {
-      case 0:
-        return styles.question_0;
-      case 1:
-        return styles.question_1;
-      case 2:
-        return styles.question_2;
-      case 3:
-        return styles.question_3;
-      case 4:
-        return styles.question_4;
-      default:
-        return 0;
-    }
-  };
-
-  const next = () => {
-    dispatch(increaseId());
-    dispatch(setAnswered());
-    dispatch(resetSeconds());
-  };
-
-  const linkOrNext = () => {
-    if (id === 4) navigate('/feedback');
-    else next();
-  };
 
   if (loading) return <Loading />;
   return (
     <>
       <TriviaTimeSide />
-      {data && (
-        <div className={styles.white_side}>
-          <Header />
-          <div className={styles.questions_main}>
-            <p className={styles.question}>
-              <span>{window.atob(data[id].question)}</span>
-            </p>
-            <ul>
-              <li>
-                <button
-                  type="button"
-                  onClick={sumScore}
-                  className={`${answeredStyle('correct')} ${styles.answer}`}
-                  disabled={answered}
-                >
-                  {window.atob(data[id].correct_answer)}
-                </button>
-              </li>
-              {data[id].incorrect_answers.map((incorrect, i) => (
-                <li key={i}>
-                  <button
-                    type="button"
-                    onClick={() => dispatch(setAnswered())}
-                    className={`${answeredStyle('incorrect')} ${styles.answer}`}
-                    disabled={answered}
-                  >
-                    {window.atob(incorrect)}
-                  </button>
-                </li>
-              ))}
-            </ul>
-            <div className={styles.progress_bar}>
-              <div className={progressBar()}></div>
-            </div>
-            {answered && (
-              <button
-                type="button"
-                onClick={linkOrNext}
-                data-testid="btn-next"
-                className={styles.next}
-              >
-                Próxima
-                <img src={nextImg} alt="proxima" />
-              </button>
-            )}
-          </div>
-        </div>
-      )}
+      <TriviaQuestionsSide />
     </>
   );
 };
